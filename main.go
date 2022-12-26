a1b23daf0a8b3ec1c52c141dbadce03aa75813d0
Binwei Fang <fangbinwei@yeah.net>
2022-12-27 06:15:44 +0800

    fix: modify retry times

--- main.go
+++ main.go
@@ -41,7 +41,7 @@ func main() {
 
 	operation.LogUploadedResult(uploaded, uploadErrs)
 
-	retried, uploadErrs:=operation.UploadRetry(uploadErrs, 5)
+	retried, uploadErrs := operation.UploadRetry(uploadErrs, 8)
 
 	uploaded = append(uploaded, retried...)
 
